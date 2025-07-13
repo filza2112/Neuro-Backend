75eb05d50931dd03f3842cbd1f5c7d9b0d99cef4
filza2112 <filza.official2112@gmail.com>
2025-07-13 12:32:51 +0530

    more

--- routes/chatRouts.js
+++ routes/chatRouts.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const chatController = require("../controllers/chatController");
+const chatController = require("../Controllers/chatController");
 const EmotionTrigger = require("../models/EmotionTrigger");
 
 router.post("/analyze", chatController.analyzeMessage);
